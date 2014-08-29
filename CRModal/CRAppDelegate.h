c9eb1efc081bf2a4b6140a93276bd2e1e324f18c
Joe Shang <shangchuanren@gmail.com>
2014-08-30 01:05:40 +0800

    Initial Commit

---
+++ CRModal/CRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRAppDelegate.h
+//  CRModal
+//
+//  Created by Joe Shang on 8/30/14.
+//  Copyright (c) 2014 Shang Chuanren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CRModal/CRViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRViewController.h
+//  CRModal
+//
+//  Created by Joe Shang on 8/30/14.
+//  Copyright (c) 2014 Shang Chuanren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRViewController : UIViewController
+
+@end
